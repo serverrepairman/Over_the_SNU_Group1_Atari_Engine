15ea2dde3bc01d226c9a957bcbea5b03b757e49d
user <user@LAPTOP-50PJO0FE>
2022-06-20 16:42:02 +0900

    V0.2

--- atari_ball/src/MovingObject/Scene.java
+++ atari_ball/src/MovingObject/Scene.java
@@ -1,5 +1,24 @@
 package MovingObject;
+import java.util.ArrayList;
+import java.awt.Graphics;
 
-public class Scene {
-
+public abstract class Scene{
+	ArrayList<MovingObject> objects;
+	public Scene(){
+		objects = new ArrayList<MovingObject>();
+	}
+	public abstract void mousePressEvent();
+	public abstract void mouseMoveEvent();
+	public abstract void mouseReleaseEvent();
+	public void update() {
+		for(MovingObject object:this.objects) {
+			object.update();
+		}
+	}
+	public void draw(Graphics g) {
+		for(MovingObject object:this.objects) {
+			object.draw(g);
+		}
+	}
+	public abstract void main(Graphics g);
 }
